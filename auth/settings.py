876db41a662c88d011c5a931fef756d3900fd2fb
shah-ka-alt <159968641+shah-ka-alt@users.noreply.github.com>
2024-03-16 16:05:55 +0530

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -144,8 +144,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR/'static']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
