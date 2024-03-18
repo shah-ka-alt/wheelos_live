e040aa8f80ade2880400dd1bbaf317110ba7e9d5
shah-ka-alt <159968641+shah-ka-alt@users.noreply.github.com>
2024-03-18 14:57:47 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,6 +23,11 @@ urlpatterns = [
     path('confirmed/', views.confirmed, name='confirmed'),
     path('profile/', views.profile, name='profile'),
     path('profileShow/', views.profileShow, name='profileShow'),
+    path('my-view/', views.my_view, name='my_view'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('password_reset_confirm/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
