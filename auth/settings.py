5bc66d2b01b7d9970d585ff7a0b47259e2bf07cf
shah-ka-alt <159968641+shah-ka-alt@users.noreply.github.com>
2024-03-19 16:25:33 +0530

    Update settings.py

--- auth/settings.py
+++ auth/settings.py
@@ -39,14 +39,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'user_agents',
 ]
 
 
 
 
 MIDDLEWARE = [
-    'myapp.middleware.MobileDesktopMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
