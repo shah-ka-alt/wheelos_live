16d9dbd40b3387a21dc38dd735028b5aa7f5ad9a
shah-ka-alt <159968641+shah-ka-alt@users.noreply.github.com>
2024-03-19 11:40:31 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.register,name='register'),
+    path('',views.landingPage, name = 'landingPage'),
+    path('register',views.register,name='register'),
     path('login',views.login,name='login'),
     path('logout',views.logout,name='logout'),
     path('display',views.display,name='display'),
